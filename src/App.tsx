17e346c3739a94ebc9dde4a2764e1435fe249c5a
Trevor Mackin <tismetrm@gmail.com>
2021-02-14 23:57:44 -0800

    Initialize Porfolio Project (Create Skeleton)

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment } from "react";
+import { Switch, Route, withRouter } from "react-router-dom";
+import NavBar from "./Components/Navigation/NavBar";
+import Home from "./Views/Home/Home";
+// import Error404 from './Views/Error404/Error404';
+// This is for testing purposes only
+// import ReduxStateTest from './Data/TestPages/ReduxStateTest';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+
+const App = () => {
+    return (
+        <Fragment>
+            <NavBar />
+            <Switch>
+                <Route exact path="/" component={Home} />
+                {/* <Route path="/productList" component={ProductsView} />
+                <Route component={Error404} /> */}
+            </Switch>
+            <style global>{`
+                /* This takes up space for the sidebar so all future centering works accordingly */
+                //     body {
+                //         margin-left: 72px;
+                //     }
+                // @media only screen and (max-width: 959px) {
+                //     body {
+                //         margin-left: 0;   
+                //     }
+                // }
+            `}</style>
+        </Fragment>
+    );
+};
 
 export default App;
