cf2b42f6a23883053651e59c6405bd9135f5ea4b
Trevor Mackin <tismetrm@gmail.com>
2021-02-18 13:30:51 -0800

    Refactor App to incorporate react router and switch statements

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
+import { Box } from "@material-ui/core";
 import React, { Fragment } from "react";
 import { Switch, Route, withRouter } from "react-router-dom";
 import NavBar from "./Components/Navigation/NavBar";
 import Home from "./Views/Home/Home";
+import Projects from "./Views/Projects/Projects";
 // import Error404 from './Views/Error404/Error404';
 // This is for testing purposes only
 // import ReduxStateTest from './Data/TestPages/ReduxStateTest';
@@ -9,25 +11,14 @@ import Home from "./Views/Home/Home";
 
 const App = () => {
     return (
-        <Fragment>
+        <Box style={{backgroundColor: '#1e2123'}}>
             <NavBar />
             <Switch>
-                <Route exact path="/" component={Home} />
-                {/* <Route path="/productList" component={ProductsView} />
-                <Route component={Error404} /> */}
+                <Route exact path="/" component={Home} /> 
+                <Route path="/projects" component={Projects} /> 
+                {/* <Route component={Error404} /> */}
             </Switch>
-            <style global>{`
-                /* This takes up space for the sidebar so all future centering works accordingly */
-                //     body {
-                //         margin-left: 72px;
-                //     }
-                // @media only screen and (max-width: 959px) {
-                //     body {
-                //         margin-left: 0;   
-                //     }
-                // }
-            `}</style>
-        </Fragment>
+        </Box>
     );
 };
 
