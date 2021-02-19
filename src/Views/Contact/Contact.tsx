87841f2d4fcf3c2f7e529a6b669fde6ef041b765
Trevor Mackin <tismetrm@gmail.com>
2021-02-18 19:21:07 -0800

    Add a Contact view that includes hyperlinks

---
+++ src/Views/Contact/Contact.tsx
@@ -0,0 +1,49 @@
+import React from "react";
+import { Box, Typography } from "@material-ui/core";
+import useStyles, { StyledIcon } from "./ContactStyles";
+
+const Contact = () => {
+  const classes = useStyles();
+
+  return (
+    <Box className={classes.root}>
+      <Box className={classes.iconsMainContainer}>
+        <a className={classes.linkStyles} href="mailto:tismetrm@gmail.com.com">
+          <Box className={classes.iconBox}>
+            <StyledIcon className="devicon-google-plain"></StyledIcon>
+            <span className={classes.spanTag}>tismeTRM@gmail.com</span>
+          </Box>
+        </a>
+        <a
+          className={classes.linkStyles}
+          href="https://www.linkedin.com/in/trevor-mackin/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Box className={classes.iconBox}>
+            <StyledIcon className="devicon-linkedin-plain"></StyledIcon>
+            <span className={classes.spanTag}>
+              linkedin.com/in/trevor-mackin
+            </span>
+          </Box>
+        </a>
+        <a
+          className={classes.linkStyles}
+          href="https://github.com/ratta2ii"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Box className={classes.iconBox}>
+            <StyledIcon className="devicon-github-original-wordmark"></StyledIcon>
+            <span className={classes.spanTag}>github.com/ratta2ii</span>
+          </Box>
+        </a>
+      </Box>
+      <Typography variant="h1" className={classes.title}>
+        Thank you for visiting!
+      </Typography>
+    </Box>
+  );
+};
+
+export default Contact;
---
+++ src/Views/Contact/ContactStyles.tsx
@@ -0,0 +1,77 @@
+import { makeStyles } from "@material-ui/core/styles";
+import styled from "styled-components";
+
+// Trying out an alternate way of styling with styled components
+export const StyledIcon = styled.i`
+  font-size: 40px;
+  color: #4caf50;
+`;
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    [theme.breakpoints.down("xs")]: {
+      padding: "0 7%",
+    },
+  },
+  iconsMainContainer: {
+    maxWidth: 430,
+    margin: "auto",
+    marginTop: 140,
+    fontSize: "1.5rem",
+    [theme.breakpoints.down("sm")]: {
+      maxWidth: 370,
+      fontSize: "1rem",
+    },
+    [theme.breakpoints.down("xs")]: {
+      marginTop: 100,
+      paddingLeft: 30,
+    },
+  },
+  iconBox: {
+    height: 60,
+    marginTop: 50,
+    display: "flex",
+    alignItems: "center",
+    [theme.breakpoints.down("xs")]: {
+      marginTop: 20,
+    },
+  },
+  title: {
+    backgroundImage:
+      "radial-gradient(circle farthest-side at 30% 50%, #f5f5f5, #616161)",
+    WebkitBackgroundClip: "text",
+    backgroundClip: "text",
+    color: "transparent",
+    fontWeight: 600,
+    fontSize: "3.5rem",
+    marginTop: 100,
+    textAlign: "center",
+    letterSpacing: ".1rem",
+    [theme.breakpoints.down("md")]: {
+      fontSize: "2.5rem",
+    },
+    [theme.breakpoints.down("sm")]: {
+      fontSize: "2.5rem",
+      marginTop: 100,
+    },
+    [theme.breakpoints.down("xs")]: {
+      marginBottom: 0,
+    },
+  },
+  spanTag: {
+    marginLeft: 30,
+    fontWeight: 400,
+    letterSpacing: ".1rem",
+    color: "#999999",
+    [theme.breakpoints.down("xs")]: {
+      marginLeft: 15,
+      fontSize: ".9rem",
+    },
+  },
+  linkStyles: {
+    textDecoration: "none",
+    color: "#bdbdbd",
+  },
+}));
+
+export default useStyles;
